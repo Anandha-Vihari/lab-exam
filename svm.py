9e80fe3b1d0c5e86792159ac9266fa35792241c8
Anandha-Vihari <notevilnorgood@gmail.com>
2026-04-30 09:36:50 +0530

    Update svm.py

--- svm.py
+++ svm.py
@@ -1,20 +1,42 @@
-
 from sklearn import svm, datasets
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import accuracy_score 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
 iris = datasets.load_iris()
 
 X = iris.data[:, :2]
 y = iris.target
 
-x_train, x_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=0
-)
+x_train, x_test, y_train, y_test = train_test_split(X, y, random_state=0, test_size=0.25)
 
-clf = svm.SVC(kernel='linear', C=1)
-clf.fit(x_train, y_train)
+clf = svm.SVC(kernel='linear', C=1).fit(x_train, y_train)
 
-pred = clf.predict(x_test)
+classifier_predictions = clf.predict(x_test)
+print(accuracy_score(y_test, classifier_predictions) * 100)
 
-print("Accuracy:", accuracy_score(y_test, pred) * 100)
+h = 0.02
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                     np.arange(y_min, y_max, h))
+
+Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.contourf(xx, yy, Z, cmap=plt.cm.coolwarm, alpha=0.3)
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm)
+
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+
+plt.xlim(xx.min(), xx.max())
+plt.ylim(yy.min(), yy.max())
+
+plt.xticks(())
+plt.yticks(())
+
+plt.title("Linear")
+plt.show()

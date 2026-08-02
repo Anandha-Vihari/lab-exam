0004f0d5b492ca6faa57bc34e61caa481d9e7254
Anandha-Vihari <notevilnorgood@gmail.com>
2026-08-02 23:17:06 +0530

    Create nlpsw.py

---
+++ nlpsw.py
@@ -0,0 +1,25 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+nltk.download("stopwords")
+nltk.download("punkt")
+nltk.download("punkt_tab")
+
+def remove_stopwords(text):
+    words = word_tokenize(text)
+    english_stopwords = set(stopwords.words("english"))
+
+    filtered_words = [
+        word for word in words
+        if word.lower() not in english_stopwords
+    ]
+
+    filtered_text = " ".join(filtered_words)
+    return filtered_text
+
+text = "NLTK is a leading platform for building Python programs to work with human language data."
+
+filtered_text = remove_stopwords(text)
+
+print(filtered_text)
